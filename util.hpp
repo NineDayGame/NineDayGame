0363fb6d5af4655424f2594e0f7efd654f7fc8a2
Ryan Fox <foxryan@gmail.com>
2011-02-21 16:15:44 -0500

    Moved the foreach stuff into util, and created macros for dynamic/static_pointer_cast

--- util.hpp
+++ util.hpp
@@ -3,6 +3,12 @@
 
 #include "camera.hpp"
 
+#include <boost/foreach.hpp>
+#define foreach BOOST_FOREACH
+
+#define DCONVERT(to,from,var) boost::dynamic_pointer_cast<to,from>((var))
+#define SCONVERT(to,from,var) boost::static_pointer_cast<to,from>((var))
+
 extern TextCamera::ShPtr print_to;
 
 void cprintf(const char* fmt, ...);
