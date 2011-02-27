1ae1e3c6b5572e28acf485ce41e058361987c276
Alex W <malexw@hotmail.com>
2011-02-27 13:09:31 -0500

    Initial prototype of the 3D targeting system.

---
+++ kobold/KoboldTargetState.hpp
@@ -0,0 +1,40 @@
+#ifndef NDG_KOBOLDTARGETSTATE_HPP
+#define NDG_KOBOLDTARGETSTATE_HPP
+
+#include <libtcod.hpp>
+#include <boost/shared_ptr.hpp>
+#include <vector>
+#include "gamestate.hpp"
+#include "glcamera.hpp"
+#include "item.hpp"
+//#include "kobold_gamestate.hpp"
+#include "renderman.hpp"
+
+class KoboldTargetState : public GameState
+{
+public:
+	typedef boost::shared_ptr<KoboldTargetState> ShPtr;
+
+	KoboldTargetState(GameState::ShPtr parent, GlCamera::ShPtr camera, RenderMan::ShPtr renderman, int x, int y);
+	virtual ~KoboldTargetState();
+
+	virtual void init();
+	
+	virtual void draw();
+	virtual void handle_input();
+	//void set_font(Font::ShPtr font);
+	void set_renderman(RenderMan::ShPtr renderman);
+	
+	RenderMan::ShPtr renderman_;
+	//Font::ShPtr font_;
+	
+	//MenuWindow::ShPtr menu_window_;
+	
+private:
+	Movable::ShPtr reticule_;
+	GlCamera::ShPtr camera_;
+	int x_, y_;
+
+};
+
+#endif
