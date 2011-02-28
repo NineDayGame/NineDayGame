9815ad0dfa199a43fa1cf6efbbf6d620d87d6f72
Ryan Fox <foxryan@gmail.com>
2011-02-27 23:57:49 -0500

    Better zombie names.

--- lore/gamestate.cpp
+++ lore/gamestate.cpp
@@ -1,8 +1,8 @@
+#include <boost/lexical_cast.hpp>
 #include "gamestate.hpp"
 #include "item.hpp"
 #include "monster.hpp"
 #include <libtcod.hpp>
-#include <stdio.h>
 
 GameState::ShPtr GameState::state = GameState::ShPtr();
 Map::ShPtr GameState::map = Map::ShPtr();
@@ -18,19 +18,26 @@ void GameState::generate_map(int width, int height)
 	map->randomize(40);
 
 	TCODRandom* rand = TCODRandom::getInstance();
+	std::string item_name = "Health Potion";
+	std::string item_desc = "A potion that restores up to 10 health.";
 	for(int i = 0; i < 50; ++i)
 	{
-		char buf[32];
-		char name[32];
-		char desc[128];
+		std::string entity_name = "Zom";
+		switch(i%5)
+		{
+		case 0: entity_name += "borg"; break;
+		case 1: entity_name += "gord"; break;
+		case 2: entity_name += "beast"; break;
+		case 3: entity_name += "berl"; break;
+		case 4: entity_name += "flord"; break;
+		}
+		entity_name += boost::lexical_cast<std::string>(i);
+		
 		int x,y;
 		map->random_free_spot(&x,&y);
-		sprintf(buf,"Entity %d",i);
-		sprintf(name,"health potion");
-		sprintf(desc,"a potion that restores up to 10 health");
 
 		TCODColor color(rand->getInt(0,255),rand->getInt(0,255),rand->getInt(0,255));
-		Monster::ShPtr e(new Monster(Map::WkPtr(map),buf,x,y,1,color,3));
+		Monster::ShPtr e(new Monster(Map::WkPtr(map),entity_name,x,y,1,color,3));
 		e->init_stats(8+rand->getInt(0,GameState::floor),
 		              3+rand->getInt(0,GameState::floor),
 		              5+rand->getInt(0,GameState::floor),
@@ -42,7 +49,7 @@ void GameState::generate_map(int width, int height)
 		if(rand->getFloat(0.0,1.0) > 0.85f)
 		{
 			TCODColor color_item(rand->getInt(0,255),rand->getInt(0,255),rand->getInt(0,255));
-			Item::ShPtr i(new Item(Map::WkPtr(map),name,desc,x,y,'I',color_item));
+			Item::ShPtr i(new Item(Map::WkPtr(map),item_name,item_desc,x,y,'I',color_item));
 			e->get(i);
 		}
 		map->get(e);
